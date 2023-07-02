77eb57353fc5c0d6e30a5c08145f4d9f53814fdd
Michael O'Brien <michaelobrien@g.harvard.edu>
2023-07-02 13:09:36 -0400

    simulator formatting

---
+++ src/jax_2dtm/simulator/pipeline.py
@@ -0,0 +1,19 @@
+"""
+Cryo-EM image rendering pipeline
+"""
+
+__all__ = ["rendering_pipeline", "log_likelihood"]
+
+
+def rendering_pipeline():
+    """
+    Return function handle for the imaging pipeline
+    """
+    pass
+
+
+def log_likelihood():
+    """
+    Log likelihood.
+    """
+    pass
