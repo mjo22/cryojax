Added documentation and renaming. Bug fixes in image rescaling, i.e. masking should be after rescaling, and need to be careful to preserve mean and variance when masking and filtering observed data.

src/jax_2dtm/io/load_volume.py
@@ -24,28 +24,32 @@ def load_grid_as_cloud(
 
     Parameters
     ----------
-    filename :
+    filename : `str`
         Path to template.
-    config :
+    config : `jax_2dtm.simulator.ImageConfig`
         Image configuration.
     real : `bool`
         If ``True``, load volume in real space.
         If ``False``, load in Fourier space.
     kwargs :
         Keyword arguments passed to
-        ``jax_2dtm.simulator.coordinatize``.
+        ``jax_2dtm.io.coordinatize``.
 
     Returns
     -------
-    cloud :
+    cloud : `jax_2dtm.simulator.Cloud`
         Point cloud generated from the 3D template.
     """
+    # Load template
     template = load_mrc(filename)
+    # Determine box_size, taking the z depth to be the longest
+    # box dimesion
     depth = max(template.shape)
     box_size = (
         jnp.array((*config.padded_shape, depth), dtype=float)
         * config.pixel_size
     )
+    # Instantiate a Cloud
     cloud = Cloud(
         *coordinatize(template, config.pixel_size, real=real, **kwargs),
         box_size,
@@ -79,29 +83,35 @@ def coordinatize(
     template: ArrayLike,
     pixel_size: float,
     real: bool = True,
+    flatten: bool = True,
     **kwargs: Any,
 ) -> tuple[Array, ...]:
     """
-    Returns flattened coordinate system and 3D volume or 2D image
-    of shape ``(N, ndim)``, where ``ndim = volume.ndim`` and
-    ``N = N1*N2*N3 - M`` or ``N = N2*N3 - M``, where ``M`` is a
-    number of points close to zero that are masked out.
-    The coordinate system is in pixel coordinates with zero
-    in the center.
+    Returns 3D volume or 2D image and its coordinate system.
+    By default, coordinates are shape ``(N, ndim)``, where
+    ``ndim = template.ndim`` and ``N = N1*N2*N3 - M`` or
+    ``N = N2*N3 - M``, where ``M`` is a number of points
+    close to zero that are masked out. The coordinate system
+    has dimensions of length with zero in the center.
 
     Parameters
     ----------
-    density : shape `(N1, N2, N3)` or `(N1, N2)`
+    template : shape `(N1, N2, N3)` or `(N1, N2)`
         3D volume or 2D image on a cartesian grid.
     pixel_size : `float`
         Camera pixel size.
     real : `bool`
-        If ``True``, return flattened density and coordinate
+        If ``True``, return density and coordinate
         system in real space. If ``False``, return density
         coordinates in Fourier space.
+    flatten : `bool`
+        If ``True``, return flattened density and coordinates,
+        which allows for optional keyword arguments for masking out
+        zero density values. If ``False``, do not flatten or
+        mask the volume.
     kwargs
         Keyword arguments passed to ``np.isclose``.
-        Disabled for ``real = False``.
+        Disabled for ``flatten = False``.
 
     Returns
     -------
@@ -112,24 +122,26 @@ def coordinatize(
     """
     ndim, shape = template.ndim, template.shape
 
-    # Mask out points where the electron density is close
-    # to zero.
-    if real:
-        flat = template.ravel()
+    # Load template in real or fourier space
+    density = template if real else fft(template)
+    # Flatten to point cloud or keep as is
+    if flatten:
+        # Mask out points where the electron density is close
+        # to zero.
+        flat = density.ravel()
         mask = np.where(~np.isclose(flat, 0.0, **kwargs))
         density = flat[mask]
     else:
-        density = fft(template)
         mask = True
 
     # Create coordinate buffer
     N = density.size
-    coords = np.zeros((N, ndim)) if real else np.zeros((*shape, ndim))
+    coords = np.zeros((N, ndim)) if flatten else np.zeros((*shape, ndim))
 
     # Generate rectangular grid and fill coordinate array
     R = fftfreqs(shape, pixel_size, real=real)
     for i in range(ndim):
-        if real:
+        if flatten:
             coords[..., i] = R[..., i].ravel()[mask]
         else:
             coords[..., i] = R[..., i]

src/jax_2dtm/simulator/intensity.py
@@ -27,14 +27,16 @@ class Intensity(Serializable):
     N: Scalar = 1.0
     mu: Scalar = 0.0
 
-    def rescale(self, image: Array) -> Array:
+    def rescale(self, image: Array, real: bool = True) -> Array:
         """
         Return the normalized image.
         """
-        return rescale_image(image, self.N, self.mu)
+        return rescale_image(image, self.N, self.mu, real=real)
 
 
-def rescale_image(image: Array, N: float, mu: float) -> Array:
+def rescale_image(
+    image: Array, N: float, mu: float, real: bool = True
+) -> Array:
     """
     Normalize so that the image is mean mu
     and standard deviation N in real space.
@@ -42,24 +44,32 @@ def rescale_image(image: Array, N: float, mu: float) -> Array:
     Parameters
     ----------
     image : `jax.Array`, shape `(N1, N2)`
-        The image in Fourier space. The zero
-        frequency component should be in the center
-        of the image.
+        The image in either real or Fourier space.
+        If in Fourier space, the zero frequency
+        component should be in the center of the image.
     N : `float`
         Intensity scale factor.
     mu : `float`
         Intensity offset.
+    real : `bool`
+        If ``True``, the given ``image`` is in real
+        space. If ``False``, it is in Fourier space.
+
     Returns
     -------
     rescaled_image : `jax.Array`, shape `(N1, N2)`
         Image rescaled to have mean ``mu`` and standard
-        deviation ``N`` in real space.
+        deviation ``N``.
     """
     N1, N2 = image.shape
     # First normalize image to zero mean and unit standard deviation
-    normalized_image = image.at[N1 // 2, N2 // 2].set(0.0)
-    normalized_image /= jnp.linalg.norm(normalized_image) / (N1 * N2)
-    rescaled_image = (
-        (normalized_image * N).at[N1 // 2, N2 // 2].set(mu * N1 * N2)
-    )
+    if real:
+        normalized_image = (image - image.mean()) / image.std()
+        rescaled_image = N * normalized_image + mu
+    else:
+        normalized_image = image.at[N1 // 2, N2 // 2].set(0.0)
+        normalized_image /= jnp.linalg.norm(normalized_image) / (N1 * N2)
+        rescaled_image = (
+            (normalized_image * N).at[N1 // 2, N2 // 2].set(mu * N1 * N2)
+        )
     return rescaled_image

src/jax_2dtm/simulator/scattering.py
@@ -48,16 +48,21 @@ class ImageConfig(Serializable):
         is the size of the desired imaging plane.
     pixel_size : `float`
         Size of camera pixels, in dimensions of length.
-    freqs : Array, shape `(N1, N2, 2)`
+    freqs : `Array`, shape `(N1, N2, 2)`
         The fourier wavevectors in the imaging plane.
-    padded_freqs : Array, shape `(M1, M2, 2)`
+    padded_freqs : `Array`, shape `(M1, M2, 2)`
         The fourier wavevectors in the imaging plane
         in the padded coordinate system.
-    coords : Array, shape `(N1, N2, 2)`
+    coords : `Array`, shape `(N1, N2, 2)`
         The coordinates in the imaging plane.
-    padded_coords : Array, shape `(M1, M2, 2)`
+    padded_coords : `Array`, shape `(M1, M2, 2)`
         The coordinates in the imaging plane
         in the padded coordinate system.
+    pad_scale : `float`
+        The scale at which to pad (or upsample) the image
+        when computing it in the object plane. This
+        should be a floating point number greater than
+        or equal to 1. By default, it is 1 (no padding).
     """
 
     shape: tuple[int, int] = field(pytree_node=False, encode=tuple)
@@ -80,8 +85,10 @@ class ImageConfig(Serializable):
     pad_scale: float = field(pytree_node=False, default=1)
 
     def __post_init__(self):
+        # Set shape after padding
         padded_shape = tuple([int(s * self.pad_scale) for s in self.shape])
         object.__setattr__(self, "padded_shape", padded_shape)
+        # Set coordinates
         freqs = jnp.asarray(fftfreqs(self.shape, self.pixel_size))
         padded_freqs = jnp.asarray(
             fftfreqs(self.padded_shape, self.pixel_size)
@@ -104,7 +111,7 @@ class ImageConfig(Serializable):
         return pad(image, self.padded_shape, **kwargs)
 
     def downsample(self, image: Array, **kwargs: Any) -> Array:
-        """Upsample an image in Fourier space."""
+        """Downsample an image in Fourier space."""
         return resize(image, self.shape, antialias=False, **kwargs)
 
     def upsample(self, image: Array, **kwargs: Any) -> Array:
@@ -115,13 +122,16 @@ class ImageConfig(Serializable):
 @dataclass
 class ScatteringConfig(ImageConfig, metaclass=ABCMeta):
     """
-    Configuration for an image with a given
+    Configuration for an image with a particular
     scattering method.
+
+    In subclasses, overwrite the ``ScatteringConfig.scatter``
+    routine.
     """
 
     @abstractmethod
-    def project(self, *args: Any):
-        """Projection method for image rendering."""
+    def scatter(self, *args: Any):
+        """Scattering method for image rendering."""
         raise NotImplementedError
 
 
@@ -134,10 +144,11 @@ class FourierSliceScattering(ScatteringConfig):
 
     order: int = field(pytree_node=False, default=1)
 
-    def project(self, *args: Any, **kwargs: Any):
+    def scatter(self, *args: Any, **kwargs: Any):
         """
-        Compute image by interpolating onto the
-        imaging plane.
+        Compute an image by sampling a slice in the
+        rotated fourier transform and interpolating onto
+        a uniform grid in the object plane.
         """
         raise NotImplementedError
         # density, coordinates, _ = args
@@ -162,7 +173,7 @@ class NufftScattering(ScatteringConfig):
 
     eps: float = field(pytree_node=False, default=1e-6)
 
-    def project(self, *args):
+    def scatter(self, *args):
         """Rasterize image with non-uniform FFTs."""
         return project_with_nufft(*args, self.padded_shape, eps=self.eps)
 
@@ -184,7 +195,7 @@ class GaussianScattering(ScatteringConfig):
 
     scale: float = field(pytree_node=False, default=1 / 3)
 
-    def project(self, *args: Any):
+    def scatter(self, *args: Any):
         """Rasterize image by integrating over Gaussians."""
         return project_with_gaussians(
             *args,
@@ -247,11 +258,11 @@ def project_with_nufft(
 
     Arguments
     ---------
-    density :
+    density : `Array`, shape `(N,)`
         Density point cloud.
-    coordinates :
+    coordinates : `Array`, shape `(N, 3)`
         Coordinate system of point cloud.
-    box_size :
+    box_size : `Array`, shape `(3,)`
         Box size of points.
     shape : `tuple[int, int]`
         Shape of the imaging plane in pixels.
@@ -284,25 +295,26 @@ def project_with_gaussians(
     scale: float,
 ) -> Array:
     """
-    Project and interpolate 3D volume point cloud
-    onto imaging plane using a non-uniform FFT.
+    Project and rasterize 3D volume onto object plane
+    by considering each pixel to be a sum of gaussians.
 
-    See ``jax_2dtm.utils.integration.integrate_gaussians`` for more detail.
+    See ``jax_2dtm.utils.integration.integrate_gaussians``
+    for more detail.
 
     Arguments
     ---------
-    density :
+    density : `Array`, shape `(N,)`
         Density point cloud.
-    coordinates :
+    coordinates : `Array`, shape `(N, 3)`
         Coordinate system of point cloud.
-    box_size :
+    box_size : `Array`, shape `(3,)`
         Box size of points.
     shape : `tuple[int, int]`
         Shape of the imaging plane in pixels.
         ``width, height = shape[0], shape[1]``
         is the size of the desired imaging plane.
     pixel_size : `float`
-        Pixel size
+        Pixel size.
     scale : `float`
         Scale of gaussians in density point cloud.
 

tests/test_serialize.py
@@ -31,4 +31,4 @@ def test_deserialize_state(scattering_model, optics_model, noisy_model):
     models = [scattering_model, optics_model, noisy_model]
     for model in models:
         state = ParameterState.from_json(model.state.to_json())
-        np.testing.assert_allclose(model.replace(state=state)(), model())
+        np.testing.assert_allclose(model.update(state=state)(), model())