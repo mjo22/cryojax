dd2bbcfb13e491f7ac581012ddc0b9c5fd1e8bb3
mjo22 <mjobrien22@gmail.com>
2023-08-08 18:58:26 -0400

    serialization pytest fails in fourier space

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -9,6 +9,7 @@ from cryojax.simulator import (
     UniformExposure,
     ParameterState,
 )
+from cryojax.utils import ifft
 
 
 def test_deserialize_state_components(noisy_model):
@@ -33,4 +34,4 @@ def test_deserialize_state(scattering_model, optics_model, noisy_model):
     models = [scattering_model, optics_model, noisy_model]
     for model in models:
         state = ParameterState.from_json(model.state.to_json())
-        np.testing.assert_allclose(model.update(state)(), model())
+        np.testing.assert_allclose(ifft(model.update(state)()), ifft(model()))
