fc5b4865dc27c7d47bf7d6158fe0714f1ccd1728
mjo22 <mjobrien22@gmail.com>
2023-09-25 18:49:05 -0400

    first pass at computing a helical net

--- src/cryojax/simulator/helix.py
+++ src/cryojax/simulator/helix.py
@@ -4,11 +4,12 @@ Abstractions of helical filaments
 
 from __future__ import annotations
 
-__all__ = ["Helix"]
+__all__ = ["Helix", "compute_lattice"]
 
 from typing import Any, Union, Annotated, Optional, Callable
 from functools import partial
 
+import jax
 import jax.numpy as jnp
 
 from .specimen import Specimen
@@ -37,24 +38,25 @@ class Helix(CryojaxObject):
     ----------
     subunit : `cryojax.simulator.Specimen`
         The helical subunit.
-    rise : `cryojax.core.Parameter` or `cryojax.core.Array`
+    rise : `Parameter` or `Array`, shape `(n_subunits,)`
         The helical rise. This has dimensions
         of length.
-    twist : `cryojax.core.Parameter` or `cryojax.core.Array`
+    twist : `Parameter` or `Array`, shape `(n_subunits,)`
         The helical twist, given in degrees if
         ``degrees = True`` and radians otherwise.
-    repeat : `cryojax.core.Array`, shape `(3,)` or `(n_repeat, 3)`
-        The displacement vector between two subunits,
-        longitudinally in contact.
-    conformations : `Conformations` or `Callable[[Lattice], Conformations]`, optional
-        The conformation of `subunit` at each lattice sitte.
+    radius : `Parameter` or `Array`, shape `(n_subunits,)`
+        The radius of the helix.
+    conformations : `Array` or `Callable[[Lattice], Array]`, optional
+        The conformation of `subunit` at each lattice site.
         This can either be a fixed set of conformations or a function
         that computes conformations based on the lattice positions.
-    n_repeat : `int`
-        The number of longitudinal repeats of the helix.
-        By default, ``1``.
+        In either case, the `Array` should be shape `(n_start*n_subunits,)`.
+    n_start : `int`
+        The start number of the helix. By default, ``1``.
+    n_subunits : `int`, optional
+        The number of subunits in the assembly.
     degrees : `bool`
-        Whether or not the helical repeat is given in
+        Whether or not the helical twist is given in
         degrees. By default, ``True``.
     lattice : `Lattice`
         The 3D cartesian lattice coordinates for each subunit.
@@ -63,12 +65,13 @@ class Helix(CryojaxObject):
     subunit: Specimen = field()
     rise: Parameter = field()
     twist: Parameter = field()
-    repeat: Parameter = field()
+    radius: Parameter = field(default=1)
     conformations: Optional[
         Union[Conformations, Callable[[Lattice], Conformations]]
     ] = field(default=None)
 
-    n_repeat: int = field(pytree_node=False, default=1)
+    n_start: int = field(pytree_node=False, default=1)
+    n_subunits: Optional[int] = field(pytree_node=False, default=None)
     degrees: bool = field(pytree_node=False, default=True)
     lattice: Lattice = field(pytree_node=False, init=False)
 
@@ -76,7 +79,14 @@ class Helix(CryojaxObject):
         object.__setattr__(
             self,
             "lattice",
-            compute_lattice(self.rise, self.twist, self.repeat, self.n_repeat),
+            compute_lattice(
+                self.rise,
+                self.twist,
+                radius=self.radius,
+                n_start=self.n_start,
+                n_subunits=self.n_subunits,
+                degrees=self.degrees,
+            ),
         )
 
     def scatter(
@@ -122,26 +132,68 @@ class Helix(CryojaxObject):
 
 
 def compute_lattice(
-    rise: float, twist: float, repeat: Array, n_repeat: int
+    rise: float,
+    twist: float,
+    radius: float = 1.0,
+    n_start: int = 1,
+    n_subunits: Optional[int] = None,
+    *,
+    degrees: bool = True,
 ) -> Lattice:
     """
-    Compute the lattice points for a given
-    helical rise and twist.
+    Compute the lattice points of a helix for a given
+    rise, twist, radius, and start number.
 
-    rise : `float`
+    Parameters
+    ----------
+    rise : `float` or `Array`, shape (n_subunits,)
         The helical rise.
-    twist : `float`
+    twist : `float` or `Array`, shape (n_subunits,)
         The helical twist.
-    repeat : `cryojax.core.Array`, shape `(3,)`
-        The displacement vector between two subunits,
-        longitudinally in contact.
-    n_repeat : `int`
-        The number of longitudinal repeats of the helix.
-        By default, ``1``.
+    radius : `float` or `Array`, shape (n_subunits,)
+        The radius of the helix.
+    n_start : `int`
+        The start number of the helix.
+    n_subunits : `int`, optional
+        The number of subunits in the assembly for
+        a single helix. The total number of subunits
+        is really equal to ``n_start * n_subunits``.
+        By default, ``2 * jnp.pi / twist``.
+    degrees : `bool`
+        Whether or not the angular parameters
+        are given in degrees or radians.
+
+    Returns
+    -------
+    lattice : `Array`, shape (n_start*n_subunits, 3)
+        The helical lattice.
     """
-    pitch = 2 * jnp.pi * rise / twist  # Helical pitch (distance between turns)
-    d = jnp.linalg.norm(repeat)
-    turns_per_repeat = int(d / pitch)  # Number of turns
-    subunits_per_repeat = int(d / rise)  # Number of points per turn
-    n_subunits = turns_per_repeat * subunits_per_repeat
-    return jnp.zeros((n_subunits, 3))
+    # Convert to radians
+    if degrees:
+        twist = jnp.deg2rad(twist)
+    # If the number of subunits is not given, compute for one helix
+    if n_subunits is None:
+        n_subunits = int(2 * jnp.pi / twist)
+    # Rotational symmetry between helices due to the start number
+    symmetry_angles = jnp.array(
+        [2 * jnp.pi * n / n_start for n in range(n_start)]
+    )
+
+    def compute_helical_coordinates(symmetry_angle):
+        """
+        Get the coordinates for a given
+        helix, where the x and y coordinates
+        are rotated by an angle.
+        """
+        theta = jnp.arange(n_subunits, dtype=float) * twist
+        x_0 = radius * jnp.cos(theta)
+        y_0 = radius * jnp.sin(theta)
+        c, s = jnp.cos(symmetry_angle), jnp.sin(symmetry_angle)
+        R = jnp.array(((c, s), (-s, c)), dtype=float)
+        x, y = R @ jnp.stack((x_0, y_0))
+        z = rise * jnp.arange(-n_subunits / 2, n_subunits / 2, 1, dtype=float)
+        return jnp.stack((x, y, z), axis=-1)
+
+    # The helical coordinates for all sub-helices
+    lattice = jax.vmap(compute_helical_coordinates)(symmetry_angles)
+    return lattice.reshape((n_start * n_subunits, 3))
