e6e41f3f4737308f526c2cd8c425c04482e79f0d
Michael O'Brien <24856964+mjo22@users.noreply.github.com>
2024-01-08 13:16:30 -0500

    add stack to field metadata

--- src/cryojax/core.py
+++ src/cryojax/core.py
@@ -17,27 +17,44 @@ import equinox as eqx
 
 def field(
     *,
+    stack: bool = False,
     **kwargs: Any,
 ) -> Any:
     """
     Add default metadata to usual dataclass fields through python
     and equinox.
+
+    Arguments
+    ---------
+    stack : `bool`
+        Metadata that indicates if a field is to be stacked
+        when giving Module fields a batch dimension. This metadata
+        is not currently supported in every cryojax Module, and is
+        only for internal cryojax batch dimension functionality.
+        In general, a user can add batch dimensions however they want.
     """
     # Equinox metadata
     static = kwargs.pop("static", False)
     if static:
+        # ... null converter
         _converter = lambda x: x
     else:
-        # This converter is necessary when a parameter is typed as,
+        # ... this converter is necessary when a parameter is typed as,
         # for example, Optional[Real_].
         _converter = (
             lambda x: jnp.asarray(x) if isinstance(x, ArrayLike) else x
         )
+    # ... set the converter to the passed converter or the default convertere
     converter = kwargs.pop("converter", _converter)
+    # Cryojax metadata
+    metadata = kwargs.pop("metadata", {})
+    # ... add the stack keyword
+    metadata["stack"] = stack
 
     return eqx.field(
         converter=converter,
         static=static,
+        metadata=metadata,
         **kwargs,
     )
 
