94e2c980fd39d7a44e6e14d340ed2351d872736b
Michael O'Brien <24856964+mjo22@users.noreply.github.com>
2024-01-28 18:29:54 -0500

    subdirectory no longer a submodule

--- src/cryojax/simulator/_assembly/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._assembly import AbstractAssembly as AbstractAssembly
-from ._helix import (
-    Helix as Helix,
-    compute_helical_lattice_positions as compute_helical_lattice_positions,
-    compute_helical_lattice_rotations as compute_helical_lattice_rotations,
-)
