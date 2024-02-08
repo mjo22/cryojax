c1ef72b2b20b473ea4679edbb711eb57eafaa1f0
Geoffrey Woollard <geoffwoollard@gmail.com>
2024-02-08 13:15:32 -0500

    jaxified fill_vec

--- src/cryojax/utils/interpolate.py
+++ src/cryojax/utils/interpolate.py
@@ -6,6 +6,7 @@ __all__ = ["interp_vec", "diff", "fill_vec"]
 
 import numpy as np
 import jax.numpy as jnp
+import jax
 
 
 def diff(xyz_rot):
@@ -134,6 +135,63 @@ def fill_vec(map_3d_interp, r0_idx_good, r1_idx_good, slice_flat_good, dd):
     return map_3d_interp
 
 
+def jaxy_fill_vec(
+    map_3d_interp, r0_idx_good, r1_idx_good, slice_flat_good, dd
+):
+    """Linear interpolation kernel.
+
+    Interpolates in nearby 8 voxels.
+    Done on good_idx indices in domain: len(good_idx)<=n_pix**2
+
+    Parameters
+    ----------
+    map_3d_interp : array
+        Empty flattened 3D map to be interpolated into/onto
+    r0_idx_good, r1_idx_good
+        Shape (3,len(good_idx))
+        Location to nearby grid points (r0 low, r1 high)
+    slice_flat_good : array
+        Shape (len(good_idx),)
+        Flattened 2D slice
+    dd : array
+        Shape (8,len(good_idx))
+        Distance to 8 nearby voxels. Linear interpolation kernel.
+
+    Returns
+    -------
+    map_3d_interp : array
+        Filled flattened 3D map to be interpolated into/onto
+    """
+    dd000, dd001, dd010, dd011, dd100, dd101, dd110, dd111 = dd
+
+    map_3d_interp = map_3d_interp.at[
+        r0_idx_good[0], r0_idx_good[1], r0_idx_good[-1]
+    ].add(slice_flat_good * dd000)
+    map_3d_interp = map_3d_interp.at[
+        r1_idx_good[0], r0_idx_good[1], r0_idx_good[-1]
+    ].add(slice_flat_good * dd001)
+    map_3d_interp = map_3d_interp.at[
+        r0_idx_good[0], r1_idx_good[1], r0_idx_good[-1]
+    ].add(slice_flat_good * dd010)
+    map_3d_interp = map_3d_interp.at[
+        r1_idx_good[0], r1_idx_good[1], r0_idx_good[-1]
+    ].add(slice_flat_good * dd011)
+    map_3d_interp = map_3d_interp.at[
+        r0_idx_good[0], r0_idx_good[1], r1_idx_good[-1]
+    ].add(slice_flat_good * dd100)
+    map_3d_interp = map_3d_interp.at[
+        r1_idx_good[0], r0_idx_good[1], r1_idx_good[-1]
+    ].add(slice_flat_good * dd101)
+    map_3d_interp = map_3d_interp.at[
+        r0_idx_good[0], r1_idx_good[1], r1_idx_good[-1]
+    ].add(slice_flat_good * dd110)
+    map_3d_interp = map_3d_interp.at[
+        r1_idx_good[0], r1_idx_good[1], r1_idx_good[-1]
+    ].add(slice_flat_good * dd111)
+
+    return map_3d_interp
+
+
 def interp_vec(slice_2d, r0, r1, dd, n_pix):
     """Linear interpolation.
 
@@ -162,6 +220,49 @@ def interp_vec(slice_2d, r0, r1, dd, n_pix):
     map_3d_interp = np.zeros((n_pix, n_pix, n_pix)).astype(slice_2d.dtype)
     count_3d_interp = np.zeros((n_pix, n_pix, n_pix))
     ones = np.ones(n_pix * n_pix)[good_idx]
+    slice_flat = slice_2d.flatten()[good_idx]
+
+    r0_idx_good = r0_idx[:, good_idx]
+    r1_idx_good = r1_idx[:, good_idx]
+
+    map_3d_interp = fill_vec(
+        map_3d_interp, r0_idx_good, r1_idx_good, slice_flat, dd[:, good_idx]
+    )
+    count_3d_interp = fill_vec(
+        count_3d_interp, r0_idx_good, r1_idx_good, ones, dd[:, good_idx]
+    )
+
+    return map_3d_interp, count_3d_interp
+
+
+def jaxy_interp_vec(slice_2d, r0, r1, dd, n_pix):
+    """Linear interpolation.
+
+    Parameters
+    ----------
+    slice_2d : array
+        Slice to be interpolated
+        Shape (n_pix,n_pix)
+
+    Returns
+    -------
+    r0,r1 : array
+        Shape (3,n_pix**2)
+        Location to nearby grid points (r0 low, r1 high)
+    dd : array
+        Shape (8,n_pix**2)
+        Distance to 8 nearby voxels. Linear interpolation kernel.
+    """
+    r0_idx = r0 + n_pix // 2
+    r1_idx = r1 + n_pix // 2
+
+    under_grid_idx = jnp.any(r0_idx < 0, axis=0)
+    over_grid_idx = jnp.any(r1_idx >= n_pix, axis=0)
+    good_idx = jnp.logical_and(~under_grid_idx, ~over_grid_idx)
+
+    map_3d_interp = jnp.zeros((n_pix, n_pix, n_pix)).astype(slice_2d.dtype)
+    count_3d_interp = jnp.zeros((n_pix, n_pix, n_pix))
+    ones = jnp.ones(n_pix * n_pix)[good_idx]
     slice_flat = slice_2d.flatten()[good_idx]
 
     r0_idx_good = r0_idx[:, good_idx]
--- tests/test_jaxify.py
+++ tests/test_jaxify.py
@@ -1,19 +1,23 @@
-from cryojax.utils.interpolate import jaxy_diff, diff
+from cryojax.utils.interpolate import diff, jaxy_diff
+from cryojax.utils.interpolate import fill_vec, jaxy_fill_vec
 from cryojax.utils.coordinates import make_coordinates
 from scipy.spatial.transform import Rotation
 import jax.numpy as jnp
 import numpy as np
+import jax
 
 
 def test_jaxy_diff():
-    xy = make_coordinates((10, 10))
+    n_pix = 10
+    xy = make_coordinates((n_pix, n_pix))
 
-    xy0 = jnp.zeros((3, 10**2))
+    xy0 = jnp.zeros((3, n_pix**2))
 
     xy0 = xy0.at[0].set(xy[:, :, 0].reshape(-1))
     xy0 = xy0.at[1].set(xy[:, :, 1].reshape(-1))
 
-    R = Rotation.from_euler("zyz", [10, 10, 0]).as_matrix()
+    deg = 10
+    R = Rotation.from_euler("zyz", [deg, deg, 0]).as_matrix()
     xyz_rot = R @ xy0
 
     r0, r1, dd = diff(xyz_rot)
@@ -30,3 +34,80 @@ def test_jaxy_diff():
     np.testing.assert_allclose(jaxy_jitted_r0, jaxy_r0)
     np.testing.assert_allclose(jaxy_jitted_r1, jaxy_r1)
     np.testing.assert_allclose(jaxy_jitted_dd, jaxy_dd)
+
+
+def test_jaxy_fill_vec():
+    """
+    Notes:
+    -----
+
+    Unsure why this these functions are different. Numerical issues for in place adding in numpy? JAX seems to be more accurate.
+    """
+
+    n_pix = 10
+    xy = make_coordinates((n_pix, n_pix))
+
+    xy0 = jnp.zeros((3, n_pix**2))
+
+    xy0 = xy0.at[0].set(xy[:, :, 0].reshape(-1))
+    xy0 = xy0.at[1].set(xy[:, :, 1].reshape(-1))
+
+    R = Rotation.from_euler("zyz", [10, 10, 0]).as_matrix()
+    xyz_rot = R @ xy0
+    r0, r1, dd = diff(xyz_rot)
+
+    slice_2d = np.ones(n_pix**2).reshape((n_pix, n_pix)).astype(np.float32)
+
+    # numpy
+    r0_idx = r0 + n_pix // 2
+    r1_idx = r1 + n_pix // 2
+
+    under_grid_idx = np.any(r0_idx < 0, axis=0)
+    over_grid_idx = np.any(r1_idx >= n_pix, axis=0)
+    good_idx = np.logical_and(~under_grid_idx, ~over_grid_idx)
+
+    map_3d_interp = np.zeros((n_pix, n_pix, n_pix)).astype(slice_2d.dtype)
+    slice_flat = slice_2d.flatten()[good_idx]
+
+    r0_idx_good = r0_idx[:, good_idx]
+    r1_idx_good = r1_idx[:, good_idx]
+
+    numpy_map_3d_interp = fill_vec(
+        map_3d_interp, r0_idx_good, r1_idx_good, slice_flat, dd[:, good_idx]
+    )
+
+    # jax
+    r0_idx = r0 + n_pix // 2
+    r1_idx = r1 + n_pix // 2
+
+    under_grid_idx = jnp.any(r0_idx < 0, axis=0)
+    over_grid_idx = jnp.any(r1_idx >= n_pix, axis=0)
+    good_idx = jnp.logical_and(~under_grid_idx, ~over_grid_idx)
+
+    map_3d_interp = jnp.zeros((n_pix, n_pix, n_pix)).astype(slice_2d.dtype)
+    slice_flat = slice_2d.flatten()[good_idx]
+
+    r0_idx_good = r0_idx[:, good_idx]
+    r1_idx_good = r1_idx[:, good_idx]
+
+    jaxy_map_3d_interp = jaxy_fill_vec(
+        map_3d_interp, r0_idx_good, r1_idx_good, slice_flat, dd[:, good_idx]
+    )
+
+    np.testing.assert_allclose(
+        numpy_map_3d_interp.sum(), jaxy_map_3d_interp.sum(), rtol=0.08
+    )
+    np.testing.assert_allclose(
+        numpy_map_3d_interp, jaxy_map_3d_interp, atol=0.6
+    )
+
+    jaxy_jitted_map_3d_interp = jax.jit(jaxy_fill_vec)(
+        map_3d_interp, r0_idx_good, r1_idx_good, slice_flat, dd[:, good_idx]
+    )
+
+    np.testing.assert_allclose(
+        numpy_map_3d_interp.sum(), jaxy_jitted_map_3d_interp.sum(), rtol=0.08
+    )
+    np.testing.assert_allclose(
+        numpy_map_3d_interp, jaxy_jitted_map_3d_interp, atol=0.6
+    )
