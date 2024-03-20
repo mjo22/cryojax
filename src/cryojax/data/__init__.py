4d431ba1d63951074b0a342a2070683308d1b077
mjo22 <mjobrien22@gmail.com>
2024-03-20 01:34:20 -0400

    reformatting

--- src/cryojax/data/__init__.py
+++ src/cryojax/data/__init__.py
@@ -6,5 +6,5 @@ from ._dataset import AbstractDataset as AbstractDataset
 from ._relion import (
     RelionDataset as RelionDataset,
     RelionParticleStack as RelionParticleStack,
-    default_relion_make_config as default_relion_make_config
+    default_relion_make_config as default_relion_make_config,
 )
