68de07d028f9151c33ba3b3db8c556c1eaaebf5d
Michael O'Brien <michaelobrien@g.harvard.edu>
2024-03-20 19:03:02 -0400

    debug and support fancy indexing in RelionDataset.__getitem__

--- src/cryojax/data/_relion.py
+++ src/cryojax/data/_relion.py
@@ -8,6 +8,7 @@ from jaxtyping import Shaped
 from typing import final, Callable, Any
 
 import equinox as eqx
+import numpy as np
 import jax.numpy as jnp
 
 from ..io import read_and_validate_starfile
@@ -38,14 +39,16 @@ class RelionParticleStack(AbstractParticleStack):
     [RELION](https://relion.readthedocs.io/en/release-5.0/).
     """
 
-    image_stack: Shaped[RealImage, "batch_dim"] = eqx.field(converter=jnp.asarray)
+    image_stack: RealImage | Shaped[RealImage, "batch_dim"] = eqx.field(
+        converter=jnp.asarray
+    )
     config: ImageConfig
     pose: EulerAnglePose
     ctf: CTF
 
     def __init__(
         self,
-        image_stack: Shaped[RealImage, "batch_dim"],
+        image_stack: RealImage | Shaped[RealImage, "batch_dim"],
         config: ImageConfig,
         pose: EulerAnglePose,
         ctf: CTF,
@@ -125,34 +128,80 @@ class RelionDataset(AbstractDataset):
         object.__setattr__(self, "make_config", make_config)
 
     @final
-    def __getitem__(self, index) -> RelionParticleStack:
+    def __getitem__(self, index: int | slice) -> RelionParticleStack:
         # Load particle data and optics group
         particle_blocks = self.data_blocks["particles"].iloc[index]
         optics_group = self.data_blocks["optics"].iloc[0]
         # Load particle image stack rlnImageName
-        relion_particle_index, image_stack_filename = particle_blocks[
-            "rlnImageName"
-        ].split("@")
-        path_to_image_stack = pathlib.Path(
-            self.path_to_relion_project, image_stack_filename
-        )
-        particle_index = int(relion_particle_index) - 1
+        image_stack_index_and_name_series_or_str = particle_blocks["rlnImageName"]
+        if isinstance(image_stack_index_and_name_series_or_str, str):
+            # In this block, the user most likely used standard indexing, like
+            # `dataset = RelionDataset(...); particle_stack = dataset[1]`
+            image_stack_index_and_name_str = image_stack_index_and_name_series_or_str
+            # ... split the whole string into its image index and filename
+            relion_particle_index, image_stack_filename = (
+                image_stack_index_and_name_str.split("@")
+            )
+            # ... create full path to the image stack
+            path_to_image_stack = pathlib.Path(
+                self.path_to_relion_project, image_stack_filename
+            )
+            # ... relion convention starts indexing at 1, not 0
+            particle_index = np.asarray(relion_particle_index, dtype=int) - 1
+        elif isinstance(image_stack_index_and_name_series_or_str, pd.Series):
+            # In this block, the user most likely used fancy indexing, like
+            # `dataset = RelionDataset(...); particle_stack = dataset[1:10]`
+            image_stack_index_and_name_series = image_stack_index_and_name_series_or_str
+            # ... split the pandas.Series into a pandas.DataFrame with two columns: one for
+            # the image index and another for the filename
+            image_stack_index_and_name_dataframe = (
+                image_stack_index_and_name_series.str.split("@", expand=True)
+            )
+            # ... get a pandas.Series for each the index and the filename
+            relion_particle_index, image_stack_filename = [
+                image_stack_index_and_name_dataframe[column]
+                for column in image_stack_index_and_name_dataframe.columns
+            ]
+            # ... multiple filenames in the same STAR file is not supported with
+            # fancy indexing
+            if image_stack_filename.nunique() != 1:
+                raise ValueError(
+                    "Found multiple image stack filenames when reading "
+                    "STAR file rows. This is most likely because you tried to "
+                    "use fancy indexing with multiple image stack filenames "
+                    "in the same STAR file. If a STAR file refers to multiple image "
+                    "stack filenames, fancy indexing is not supported. For example, this will "
+                    "raise an error: `dataset = RelionDataset(...); particle_stack = dataset[1:10]`."
+                )
+            # ... create full path to the image stack
+            path_to_image_stack = pathlib.Path(
+                self.path_to_relion_project, image_stack_filename[0]
+            )
+            # ... relion convention starts indexing at 1, not 0
+            particle_index = (
+                np.asarray(relion_particle_index.astype(int), dtype=int) - 1
+            )
+        else:
+            raise IOError(
+                "Could not read `rlnImageName` in STAR file for `RelionDataset` "
+                f"index equal to {index}."
+            )
         with mrcfile.mmap(path_to_image_stack, mode="r", permissive=True) as mrc:
-            image_stack = mrc.data[particle_index]  # type: ignore
+            image_stack = np.asarray(mrc.data[particle_index])  # type: ignore
         # Read metadata into a RelionParticleStack
         # ... particle data
-        defocus_u_in_angstroms = particle_blocks["rlnDefocusU"]
-        defocus_v_in_angstroms = particle_blocks["rlnDefocusV"]
-        astigmatism_angle = particle_blocks["rlnDefocusAngle"]
-        phase_shift = particle_blocks["rlnPhaseShift"]
+        defocus_u_in_angstroms = np.asarray(particle_blocks["rlnDefocusU"])
+        defocus_v_in_angstroms = np.asarray(particle_blocks["rlnDefocusV"])
+        astigmatism_angle = np.asarray(particle_blocks["rlnDefocusAngle"])
+        phase_shift = np.asarray(particle_blocks["rlnPhaseShift"])
         # ... optics group data
-        image_size = optics_group["rlnImageSize"]
-        pixel_size = optics_group["rlnImagePixelSize"]
-        voltage_in_kilovolts = optics_group["rlnVoltage"]
-        spherical_aberration_in_mm = optics_group["rlnVoltage"]
-        amplitude_contrast_ratio = optics_group["rlnAmplitudeContrast"]
+        image_size = np.asarray(optics_group["rlnImageSize"])
+        pixel_size = np.asarray(optics_group["rlnImagePixelSize"])
+        voltage_in_kilovolts = np.asarray(optics_group["rlnVoltage"])
+        spherical_aberration_in_mm = np.asarray(optics_group["rlnVoltage"])
+        amplitude_contrast_ratio = np.asarray(optics_group["rlnAmplitudeContrast"])
         # ... create cryojax objects
-        config = self.make_config((image_size, image_size), pixel_size)
+        config = self.make_config((int(image_size), int(image_size)), pixel_size)
         ctf = CTF(
             defocus_u_in_angstroms,
             defocus_v_in_angstroms,
@@ -163,7 +212,8 @@ class RelionDataset(AbstractDataset):
             phase_shift,
         )
         pose = EulerAnglePose()
-        # ... fill objects with optional keys
+        # ... values for the pose are optional, so look to see if
+        # each key is present
         particle_keys = particle_blocks.keys()
         pose_parameter_names_and_values = []
         if "rlnOriginXAngst" in particle_keys:
@@ -183,16 +233,41 @@ class RelionDataset(AbstractDataset):
                 ("view_theta", particle_blocks["rlnAngleTilt"])
             )
         if "rlnAnglePsi" in particle_keys:
+            # Relion uses -999.0 as a placeholder for an un-estimated in-plane
+            # rotation
+            if isinstance(particle_blocks["rlnAnglePsi"], pd.Series):
+                # ... check if all values are equal to -999.0. If so, just
+                # replace the whole pandas.Series with 0.0
+                if (
+                    particle_blocks["rlnAnglePsi"].nunique() == 1
+                    and particle_blocks["rlnAnglePsi"][0] == -999.0
+                ):
+                    particle_blocks_for_psi = 0.0
+                # ... otherwise, replace -999.0 values with 0.0
+                else:
+                    particle_blocks_for_psi = particle_blocks["rlnAnglePsi"].where(
+                        lambda x: x != -999.0, 0.0
+                    )
+            else:
+                # ... if the column is just equal to a float, then
+                # directly check if it is equal to -999.0
+                particle_blocks_for_psi = (
+                    0.0
+                    if particle_blocks["rlnAnglePsi"] == -999.0
+                    else particle_blocks["rlnAnglePsi"]
+                )
             pose_parameter_names_and_values.append(
-                ("view_psi", particle_blocks["rlnAnglePsi"])
+                ("view_psi", particle_blocks_for_psi)
             )
         pose_parameter_names, pose_parameter_values = tuple(
             zip(*pose_parameter_names_and_values)
         )
+        # ... fill the EulerAnglePose will keys that are present. if they are not
+        # present, keep the default values in the `pose = EulerAnglePose()` instantiation
         pose = eqx.tree_at(
             lambda p: tuple([getattr(p, name) for name in pose_parameter_names]),
             pose,
-            tuple(pose_parameter_values),
+            tuple([jnp.asarray(value) for value in pose_parameter_values]),
         )
 
         return RelionParticleStack(image_stack, config, pose, ctf)
