4d1aff89dfe4fd0a32da399a12452622f1ee1f01
Michael O'Brien <michaelobrien@g.harvard.edu>
2024-03-23 15:18:23 -0400

    remove strict jax config

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,6 @@ from cryojax.io import read_array_with_spacing_from_mrc
 
 
 jax.config.update("jax_enable_x64", True)
-jax.config.update("jax_numpy_dtype_promotion", "strict")
-jax.config.update("jax_numpy_rank_promotion", "raise")
 
 
 @pytest.fixture
