3b3e99ca234b2e0106212727e25eeeecd9588f26
Michael O'Brien <michaelobrien@g.harvard.edu>
2024-06-12 17:25:53 -0400

    simplify the signal scale factor

--- src/cryojax/inference/distributions/_gaussian_distributions.py
+++ src/cryojax/inference/distributions/_gaussian_distributions.py
@@ -10,7 +10,6 @@ import jax.random as jr
 from jaxtyping import Array, Complex, Float, PRNGKeyArray
 
 from ..._errors import error_if_not_positive
-from ...image import rescale_image
 from ...image.operators import Constant, FourierOperatorLike
 from ...simulator import AbstractImagingPipeline
 from ._base_distribution import AbstractDistribution
@@ -38,16 +37,7 @@ class IndependentGaussianFourierModes(AbstractDistribution, strict=True):
         - `imaging_pipeline`: The image formation model.
         - `variance_function`: The variance of each fourier mode. By default,
                                `cryojax.image.operators.Constant(1.0)`.
-        - `signal_scale_factor`: A scale factor for the standard deviation of the
-                                 underlying signal simulated from `imaging_pipeline`.
-                                 The standard deviation of the signal is rescaled to be
-                                 equal to `signal_scale_factor / jnp.sqrt(n_pixels)`,
-                                 where the inverse square root of `n_pixels` is included
-                                 so that the scale of the signal does not depend on the
-                                 number of pixels. As a result, a good starting value for
-                                 `signal_scale_factor` should be on the order of the
-                                 extent of the object in pixels. By default,
-                                 `signal_scale_factor = sqrt(imaging_pipeline.instrument_config.n_pixels)`.
+        - `signal_scale_factor`: A scale factor for the underlying signal simulated from `imaging_pipeline`.
         """  # noqa: E501
         self.imaging_pipeline = imaging_pipeline
         self.variance_function = variance_function or Constant(1.0)
@@ -73,16 +63,39 @@ class IndependentGaussianFourierModes(AbstractDistribution, strict=True):
         ]
     ):
         """Render the image formation model."""
-        n_pixels = self.imaging_pipeline.instrument_config.n_pixels
-        shape = self.imaging_pipeline.instrument_config.shape
         simulated_image = self.imaging_pipeline.render(get_real=get_real)
-        return rescale_image(
-            simulated_image,
-            std=self.signal_scale_factor / jnp.sqrt(n_pixels),
-            mean=0.0,
-            is_real=get_real,
-            shape_in_real_space=shape,
+        return self.signal_scale_factor * simulated_image
+
+    def compute_noise(
+        self, rng_key: PRNGKeyArray, *, get_real: bool = True
+    ) -> (
+        Float[
+            Array,
+            "{self.imaging_pipeline.instrument_config.y_dim} "
+            "{self.imaging_pipeline.instrument_config.x_dim}",
+        ]
+        | Complex[
+            Array,
+            "{self.imaging_pipeline.instrument_config.y_dim} "
+            "{self.imaging_pipeline.instrument_config.x_dim//2+1}",
+        ]
+    ):
+        pipeline = self.imaging_pipeline
+        freqs = pipeline.instrument_config.padded_frequency_grid_in_angstroms
+        # Compute the zero mean variance and scale up to be independent of the number of
+        # pixels
+        padded_n_pixels = pipeline.instrument_config.padded_n_pixels
+        std = jnp.sqrt(padded_n_pixels * self.variance_function(freqs))
+        noise = pipeline.postprocess(
+            std
+            * jr.normal(rng_key, shape=freqs.shape[0:-1])
+            .at[0, 0]
+            .set(0.0)
+            .astype(complex),
+            get_real=get_real,
         )
+
+        return noise
 
     @override
     def sample(
@@ -100,22 +113,9 @@ class IndependentGaussianFourierModes(AbstractDistribution, strict=True):
         ]
     ):
         """Sample from the gaussian noise model."""
-        pipeline = self.imaging_pipeline
-        freqs = pipeline.instrument_config.padded_frequency_grid_in_angstroms
-        # Compute the zero mean variance and scale up to be independent of the number of
-        # pixels
-        padded_n_pixels = pipeline.instrument_config.padded_n_pixels
-        std = jnp.sqrt(padded_n_pixels * self.variance_function(freqs))
-        noise = pipeline.postprocess(
-            std
-            * jr.normal(rng_key, shape=freqs.shape[0:-1])
-            .at[0, 0]
-            .set(0.0)
-            .astype(complex),
-            get_real=get_real,
+        return self.compute_signal(get_real=get_real) + self.compute_noise(
+            rng_key, get_real=get_real
         )
-        image = self.compute_signal(get_real=get_real)
-        return image + noise
 
     @override
     def log_likelihood(
