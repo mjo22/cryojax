500d9c166311f2c4163656637a6955daff030803
Michael O'Brien <michaelobrien@g.harvard.edu>
2024-08-06 13:53:58 -0400

    pass analytical fourier density

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,14 +1,16 @@
 import jax.numpy as jnp
 import numpy as np
-# import pytest
+from matplotlib import pyplot as plt
 
+# import pytest
 ## plotting settings
 import cryojax.simulator as cxs
 from cryojax.constants import convert_keV_to_angstroms
-from cryojax.coordinates import make_coordinate_grid, make_frequency_slice
-from cryojax.image import irfftn
-
-from matplotlib import pyplot as plt
+from cryojax.coordinates import (
+    make_coordinate_grid,
+    make_frequency_grid,
+    make_frequency_slice,
+)
 
 
 # @pytest.mark.parametrize(
@@ -19,44 +21,49 @@ from matplotlib import pyplot as plt
 #     ],
 # )
 def test_fourier_slice_interpolation(phi, theta, psi):
-    
     ## Create a single Gaussian centered in the middle for real space
     sigma = 10.0
+    sigma_fourier = 1 / sigma
     npts = 101
     voxel_size = 1.0
     xyz = make_coordinate_grid((npts, npts, npts), voxel_size)
-    r = np.sqrt(xyz[:,:,:,0] ** 2 + xyz[:,:,:,1] ** 2 + xyz[:,:,:,2] ** 2)
-    density_3d_true = np.exp(-(r**2) / (2 * sigma**2))
+    xyz_fourier = jnp.fft.fftshift(
+        make_frequency_grid((npts, npts, npts), voxel_size, half_space=False)
+    )
+    r = jnp.linalg.norm(xyz, axis=-1)
+    r_fourier = r = jnp.linalg.norm(xyz_fourier, axis=-1)
+    density_3d_true = jnp.exp(-(r**2) / (2 * sigma**2))
+    fourier_density_3d_true = jnp.exp(-(r_fourier**2) / (2 * sigma_fourier**2))
 
     # Instantiate API
     # pose = cxs.EulerAnglePose(view_phi=phi, view_theta=theta, view_psi=psi)
-    frequency_slice = make_frequency_slice((npts, npts), voxel_size, half_space = False)
-    print(frequency_slice.shape)
-    voxel_potential = cxs.FourierVoxelGridPotential(density_3d_true, frequency_slice, voxel_size)
-    # voxel_potential_in_lab_frame = voxel_potential.rotate_to_pose(pose)
+    frequency_slice_in_pixels = make_frequency_slice((npts, npts), half_space=False)
+    print(frequency_slice_in_pixels.shape)
     potential_integrator = cxs.FourierSliceExtraction(interpolation_order=1)
     fourier_slice_with_zero_in_corner = (
         potential_integrator.extract_voxels_from_grid_points(
-            voxel_potential.fourier_voxel_grid,
-            voxel_potential.frequency_slice_in_pixels,
-            voxel_potential.voxel_size,
+            fourier_density_3d_true.astype(complex),
+            frequency_slice_in_pixels,
+            voxel_size,
             wavelength_in_angstroms=convert_keV_to_angstroms(300.0),
         )
     )
     image_fourier = jnp.fft.fftshift(fourier_slice_with_zero_in_corner, axes=(0,))
     # image_real = irfftn(fourier_slice_with_zero_in_corner, s=(npts, npts))
     # print(image_fourier.shape, image_fourier.dtype)
-    
+
     # plt.imshow(image_fourier.real)
     # plt.savefig("./plots/test_map_coordinates_1.png")
-    
-    xyz_slice = voxel_potential.frequency_slice_in_pixels[0]
+
+    xyz_slice = frequency_slice_in_pixels[0]
     # print(xyz_slice)
     # xyz_slice = xyz_slice.reshape(-1, 3)
-    
-    r_slice = np.sqrt(xyz_slice[:,:,0] ** 2 + xyz_slice[:,:,1] ** 2 + xyz_slice[:,:,2] ** 2)
-    slice_density_true = np.exp(-(r_slice ** 2) / (2 * sigma**2))
-    
+
+    r_slice = np.sqrt(
+        xyz_slice[:, :, 0] ** 2 + xyz_slice[:, :, 1] ** 2 + xyz_slice[:, :, 2] ** 2
+    )
+    slice_density_true = np.exp(-(r_slice**2) / (2 * sigma**2))
+
     fig = plt.figure()
     ax1 = fig.add_subplot(131)
     ax1.imshow(density_3d_true[:, :, 50])
@@ -65,8 +72,6 @@ def test_fourier_slice_interpolation(phi, theta, psi):
     ax3 = fig.add_subplot(133)
     ax3.imshow(image_fourier.real)
     plt.savefig("./plots/test_map_coordinates_2.png")
-    
-    
 
 
 if __name__ == "__main__":
