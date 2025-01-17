aa11d054848c52f90151a56514eafc8dacd50f94
Michael O'Brien <michaelobrien@g.harvard.edu>
2025-01-16 21:23:50 -0500

    missed docstrng

--- src/cryojax/coordinates/_geometry.py
+++ src/cryojax/coordinates/_geometry.py
@@ -14,6 +14,12 @@ def cartesian_to_polar(
     - `square`:
         If `True`, return the square of the radial coordinate
         $|r|^2$. Otherwise, return $|r|$.
+
+    **Returns:**
+
+    A tuple `(r, theta)`, where `r` is the radial coordinate system and
+    `theta` is the angular coordinate system. If `square=True`, return a
+    tuple `(r_squared, theta)`.
     """
     theta = jnp.arctan2(
         coordinate_or_frequency_grid[..., 0], coordinate_or_frequency_grid[..., 1]
