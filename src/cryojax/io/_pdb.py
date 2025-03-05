13ec9d9d025e5d1b83bac9a8600252564895e84b
DSilva27 <david.silva@yale.edu>
2025-03-05 11:05:41 -0500

    fix typing in get_b_factors

--- src/cryojax/io/_pdb.py
+++ src/cryojax/io/_pdb.py
@@ -52,7 +52,7 @@ def read_atoms_from_pdb(
     atom_filter: str = "all",
     is_assembly: bool = False,
     i_model: Optional[int] = None,
-    get_b_factors: Literal[False] = False,
+    get_b_factors: bool = False,
     *,
     standard_names: bool = True,
     top: Optional[mdtraj.core.topology.Topology] = None,
