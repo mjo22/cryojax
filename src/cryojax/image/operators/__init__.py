24652f77b673a340a9d8d60ee8d18b1b1e499edb
shuyuvankerkwijk <shuyuwuvk@gmail.com>
2025-03-06 15:18:54 -0800

    Fixed init files

--- src/cryojax/image/operators/__init__.py
+++ src/cryojax/image/operators/__init__.py
@@ -14,7 +14,6 @@ from ._fourier_operator import (
     FourierOperatorLike as FourierOperatorLike,
     Lorenzian as Lorenzian,
     ZeroMode as ZeroMode,
-    Parkhurst2024_Gaussian as Parkhurst2024_Gaussian,
 )
 from ._masks import (
     AbstractMask as AbstractMask,
