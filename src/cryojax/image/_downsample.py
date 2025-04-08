d95056fc7d1487a62739c905959c9e134e5fc201
DSilva27 <david.silva@yale.edu>
2025-04-08 17:19:42 -0400

    update downsampling to use Hartley transform for real signals

--- src/cryojax/image/_downsample.py
+++ src/cryojax/image/_downsample.py
@@ -3,7 +3,7 @@
 from typing import overload
 
 import jax.numpy as jnp
-from jaxtyping import Array, Inexact
+from jaxtyping import Array, Complex, Float, Inexact
 
 from ._edges import crop_to_shape
 from ._fft import fftn, ifftn
@@ -13,7 +13,6 @@ from ._fft import fftn, ifftn
 def downsample_with_fourier_cropping(
     image_or_volume: Inexact[Array, "_ _ _"],
     downsampling_factor: float | int,
-    get_real: bool = True,
 ) -> Inexact[Array, "_ _ _"]: ...
 
 
@@ -21,14 +20,12 @@ def downsample_with_fourier_cropping(
 def downsample_with_fourier_cropping(
     image_or_volume: Inexact[Array, "_ _"],
     downsampling_factor: float | int,
-    get_real: bool = True,
 ) -> Inexact[Array, "_ _"]: ...
 
 
 def downsample_with_fourier_cropping(
     image_or_volume: Inexact[Array, "_ _"] | Inexact[Array, "_ _ _"],
     downsampling_factor: float | int,
-    get_real: bool = True,
 ) -> Inexact[Array, "_ _"] | Inexact[Array, "_ _ _"]:
     """Downsample an array using fourier cropping.
 
@@ -38,8 +35,6 @@ def downsample_with_fourier_cropping(
     - `downsample_factor`:
         A scale factor at which to downsample `image_or_volume`
         by. Must be a value greater than `1`.
-    - `get_real`:
-        If `False`, the `image_or_volume` is returned in fourier space.
 
     **Returns:**
 
@@ -55,9 +50,7 @@ def downsample_with_fourier_cropping(
             int(image.shape[0] / downsampling_factor),
             int(image.shape[1] / downsampling_factor),
         )
-        downsampled_array = downsample_to_shape_with_fourier_cropping(
-            image, new_shape, get_real=get_real
-        )
+        downsampled_array = downsample_to_shape_with_fourier_cropping(image, new_shape)
     elif image_or_volume.ndim == 3:
         volume = image_or_volume
         new_shape = (
@@ -65,30 +58,20 @@ def downsample_with_fourier_cropping(
             int(volume.shape[1] / downsampling_factor),
             int(volume.shape[2] / downsampling_factor),
         )
-        downsampled_array = downsample_to_shape_with_fourier_cropping(
-            volume, new_shape, get_real=get_real
-        )
+        downsampled_array = downsample_to_shape_with_fourier_cropping(volume, new_shape)
     else:
         raise ValueError(
             "`downsample_with_fourier_cropping` can only crop images and volumes. "
             f"Got an array with number of dimensions {image_or_volume.ndim}."
         )
 
-    if get_real:
-        return (
-            downsampled_array.real
-            if jnp.issubdtype(image_or_volume.dtype, jnp.floating)
-            else downsampled_array
-        )
-    else:
-        return downsampled_array
+    return downsampled_array
 
 
 @overload
 def downsample_to_shape_with_fourier_cropping(
     image_or_volume: Inexact[Array, "_ _"],
     downsampled_shape: tuple[int, int],
-    get_real: bool = True,
 ) -> Inexact[Array, "_ _"]: ...
 
 
@@ -96,24 +79,26 @@ def downsample_to_shape_with_fourier_cropping(
 def downsample_to_shape_with_fourier_cropping(
     image_or_volume: Inexact[Array, "_ _ _"],
     downsampled_shape: tuple[int, int, int],
-    get_real: bool = True,
 ) -> Inexact[Array, "_ _ _"]: ...
 
 
 def downsample_to_shape_with_fourier_cropping(
     image_or_volume: Inexact[Array, "_ _"] | Inexact[Array, "_ _ _"],
     downsampled_shape: tuple[int, int] | tuple[int, int, int],
-    get_real: bool = True,
 ) -> Inexact[Array, "_ _"] | Inexact[Array, "_ _ _"]:
     """Downsample an array to a specified shape using fourier cropping.
+
+    For real signals the Hartley Transform is used to downsample the signal.
+    For complex signals the Fourier Transform is used to downsample the signal.
+
+    The real case is based on the `downsample_transform` function in cryoDRGN
+    https://github.com/ml-struct-bio/cryodrgn/blob/4ba75502d4dd1d0e5be3ecabf4a005c652edf4b5/cryodrgn/commands/downsample.py#L154
 
     **Arguments:**
 
     - `image_or_volume`: The image or volume array to downsample.
     - `downsampled_shape`:
         The new shape after fourier cropping.
-    - `get_real`:
-        If `False`, the `image_or_volume` is returned in fourier space.
 
     **Returns:**
 
@@ -122,11 +107,44 @@ def downsample_to_shape_with_fourier_cropping(
     the downsampled array in fourier space assuming hermitian symmetry,
     with the zero frequency component in the corner.
     """
-    fourier_array = jnp.fft.fftshift(fftn(image_or_volume))
-    cropped_fourier_array = crop_to_shape(fourier_array, downsampled_shape)
-    if get_real:
-        return ifftn(jnp.fft.ifftshift(cropped_fourier_array))
+    if jnp.iscomplexobj(image_or_volume):
+        return _downsample_complex_signal_to_shape_with_fourier_cropping(
+            image_or_volume, downsampled_shape
+        )
     else:
-        return jnp.fft.ifftshift(cropped_fourier_array)[
-            ..., : downsampled_shape[-1] // 2 + 1
-        ]
+        return _downsample_real_signal_to_shape_with_fourier_cropping(
+            image_or_volume, downsampled_shape
+        )
+
+
+def _downsample_real_signal_to_shape_with_fourier_cropping(
+    image_or_volume: Float[Array, "_ _"] | Float[Array, "_ _ _"],
+    downsampled_shape: tuple[int, int] | tuple[int, int, int],
+) -> Float[Array, "_ _"] | Float[Array, "_ _ _"]:
+    # Forward Hartley Transform
+    hartley_array = jnp.fft.fftshift(fftn(image_or_volume))
+    hartley_array = hartley_array.real - hartley_array.imag
+
+    # Crop to the desired shape
+    ds_image_or_volume = crop_to_shape(hartley_array, downsampled_shape)
+
+    # Inverse Hartley Transform
+    ds_image_or_volume = jnp.fft.fftshift(fftn(ds_image_or_volume))
+    ds_image_or_volume /= ds_image_or_volume.size
+    ds_image_or_volume = ds_image_or_volume.real - ds_image_or_volume.imag
+
+    return ds_image_or_volume
+
+
+def _downsample_complex_signal_to_shape_with_fourier_cropping(
+    image_or_volume: Complex[Array, "_ _"] | Complex[Array, "_ _ _"],
+    downsampled_shape: tuple[int, int] | tuple[int, int, int],
+) -> Complex[Array, "_ _"] | Complex[Array, "_ _ _"]:
+    # Forward Hartley Transform
+    fourier_array = jnp.fft.fftshift(fftn(image_or_volume))
+
+    # Crop to the desired shape
+    cropped_fourier_array = crop_to_shape(fourier_array, downsampled_shape)
+
+    # Inverse Hartley Transform
+    return ifftn(jnp.fft.ifftshift(cropped_fourier_array))
