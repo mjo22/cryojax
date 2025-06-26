3e7d9e8ac9cd63547ce927cf168239bb48142fb4
Michael O'Brien <michaelobrien@g.harvard.edu>
2025-06-26 00:40:28 -0400

    fix typo

--- src/cryojax/simulator/_potential_integrator/atom_potential_integrator.py
+++ src/cryojax/simulator/_potential_integrator/atom_potential_integrator.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 import jax.scipy as jsp
 from jaxtyping import Array, Complex, Float
 
-from ...constants._conventions import convert_variance_to_b_factor
+from ...constants import convert_variance_to_b_factor
 from ...coordinates import make_1d_coordinate_grid
 from ...image import (
     downsample_to_shape_with_fourier_cropping,
